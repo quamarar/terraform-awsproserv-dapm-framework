6e6d884eb8938e70f2d12a835e94aa8da98b843d
quamarar <122767728+quamarar@users.noreply.github.com>
2023-10-06 17:35:58 +0530

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -19,7 +19,7 @@ module "batch_executioner_role" {
 /* ----------------------- Glue permissions ---------------------- */
 
 module "glue_custom_iam_policy" {
-  source = "github.com/quamarar/terraform-common-modules//iam-policy"
+  source = "github.com/quamarar/terraform-common-modules//iam-policy?ref=master""
 
   name   = "${var.name_prefix}-dapf-${local.context}-glue-job-policy"
   path   = "/"
